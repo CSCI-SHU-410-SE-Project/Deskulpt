314d9463e5b6d59d5849059e896ebc97ca0117d7
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-03-29 14:38:04 +0800

    MAINT FIX missing favicon and restore icons (#2)
    * MAINT fix missing favicon and restore icons
    
    * fix npm build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => ({
@@ -9,6 +10,13 @@ export default defineConfig(() => ({
     strictPort: true,
     watch: {
       ignored: ["**/src-tauri/**"],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "views/index.html"),
+      },
     },
   },
 }));
