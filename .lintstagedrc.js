e14aea49c3bca0bbbdf373c23511bb07cc639f00
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-01 17:14:14 +0800

    refined comment

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -12,9 +12,10 @@ export default {
     const fileArgs = quote(filenames);
     return [`prettier --write ${fileArgs}`];
   },
-  // Note that cargo-clippy is not run because it will need to compile the project,
-  // which is not a good idea for a pre-commit hook; we shall rely on CI to discover
-  // potential cargo-clippy issues
+  // Note that cargo-clippy is not run because it will need to compile the project and
+  // cannot be limited only to staged files, making it not a good idea for a pre-commit
+  // hook; we shall rely on CI to discover cargo-clippy issues and recommend users to
+  // manually fix or use `npm run lint rs lint -- --fix`
   "**/*.rs": (filenames) => {
     const fileArgs = quote(filenames);
     return [`rustfmt -- ${fileArgs}`];
