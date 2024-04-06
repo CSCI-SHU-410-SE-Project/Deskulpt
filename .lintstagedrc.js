44bef209bc701f8162740535a754e0b5f73a40d1
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-06 16:52:53 +0800

    pre-commit tty not found and escape error for win cmd

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,19 +1,27 @@
 import { quote } from "shell-quote";
 
+const isWin = process.platform === "win32";
+
 export default {
   "**/*.{js,jsx,mjs,ts,tsx,mts}": (filenames) => {
-    const fileArgs = quote(filenames);
+    const fileArgs = escape(filenames);
     return [
       `prettier --write ${fileArgs}`,
       `eslint --max-warnings=0 --fix ${fileArgs}`,
     ];
   },
   "**/*.{json,json5,md,html,css,scss,yml,yaml}": (filenames) => {
-    const fileArgs = quote(filenames);
+    const fileArgs = escape(filenames);
     return [`prettier --write ${fileArgs}`];
   },
   "**/*.rs": (filenames) => {
-    const fileArgs = quote(filenames);
+    const fileArgs = escape(filenames);
     return [`rustfmt -- ${fileArgs}`];
   },
 };
+
+function escape(filenames) {
+  return filenames
+    .map((filename) => (isWin ? `"${filename}"` : quote([filename])))
+    .join(" ");
+}
