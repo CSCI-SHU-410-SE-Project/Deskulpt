892d1e453e002a5d80e74c5294e5d28aa0757078
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-06 17:00:12 +0800

    MAINT `cargo-clippy` is not included in `pre-commit` and ignore `eslint` warning for ignored files (#5)
    * MAINT add comment why cargo-clippy is not a pre-commit hook
    
    * refined comment
    
    * ignore 'ignore' warnings of eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,13 +5,17 @@ export default {
     const fileArgs = quote(filenames);
     return [
       `prettier --write ${fileArgs}`,
-      `eslint --max-warnings=0 --fix ${fileArgs}`,
+      `eslint --max-warnings=0 --no-warn-ignored --fix ${fileArgs}`,
     ];
   },
   "**/*.{json,json5,md,html,css,scss,yml,yaml}": (filenames) => {
     const fileArgs = quote(filenames);
     return [`prettier --write ${fileArgs}`];
   },
+  // Note that cargo-clippy is not run because it will need to compile the project and
+  // cannot be limited only to staged files, making it not a good idea for a pre-commit
+  // hook; we shall rely on CI to discover cargo-clippy issues and recommend users to
+  // manually fix or use `npm run lint rs lint -- --fix`
   "**/*.rs": (filenames) => {
     const fileArgs = quote(filenames);
     return [`rustfmt -- ${fileArgs}`];
