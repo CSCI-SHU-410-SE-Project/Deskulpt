a58a0f1d602cb14be3f215e9572858afacf2db5d
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-12 20:30:06 +0800

    remove tests; very minor modifications that hardly touch logic

--- src-tauri/src/widget_api/fs/mod.rs
+++ src-tauri/src/widget_api/fs/mod.rs
@@ -1,13 +1,13 @@
 mod apis;
-pub mod utils;
+pub(crate) mod utils;
 
 use tauri::{
     plugin::{Builder, TauriPlugin},
     Runtime,
 };
 
-/// The registered APIs can be invoked as "plugin:widget_api.fs|<api_name>".
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
+/// The registered APIs can be invoked as `plugin:widget_api.fs|<api_name>``.
+pub(crate) fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("widget_api.fs")
         .invoke_handler(tauri::generate_handler![
             apis::is_file,
