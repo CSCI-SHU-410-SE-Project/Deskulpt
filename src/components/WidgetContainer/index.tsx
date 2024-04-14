5e3954befabacd16ade8f85900de3f218f4ede9e
Frank Zhu <rz2606@nyu.edu>
2024-04-14 22:11:29 +0800

    [updated] make div draggable

--- src/components/WidgetContainer/index.tsx
+++ src/components/WidgetContainer/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Tooltip } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import React from "react";
-
+import Draggable from "react-draggable";
 /**
  * The widget container component.
  *
@@ -19,29 +19,30 @@ export default function WidgetContainer(props: {
 
   return (
     <React.StrictMode>
-      <Box
-        sx={{
-          px: 2,
-          py: 1,
-          m: 1,
-          borderRadius: 1,
-          bgcolor: "lightblue",
-          position: "relative",
-        }}
-      >
-        <Tooltip title={id} placement="left">
-          <InfoIcon
-            sx={{
-              position: "absolute",
-              top: 5,
-              right: 5,
-              zIndex: 2000,
-              fontSize: 15,
-            }}
-          />
-        </Tooltip>
-        {inner}
-      </Box>
+      <Draggable>
+        <Box
+          sx={{
+            px: 2,
+            py: 1,
+            m: 1,
+            borderRadius: 1,
+            // bgcolor: "lightblue",
+            position: "relative",
+          }}
+        >
+          <Tooltip title={id} placement="left">
+            <InfoIcon
+              sx={{
+                position: "absolute",
+                top: 5,
+                zIndex: 2000,
+                fontSize: 15,
+              }}
+            />
+          </Tooltip>
+          {inner}
+        </Box>
+      </Draggable>
     </React.StrictMode>
   );
 }
