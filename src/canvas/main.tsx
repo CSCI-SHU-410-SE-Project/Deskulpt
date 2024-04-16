e48c9d79917538e7ba342042a3b07e268b8a91da
Xinyu Li <eigerli@outlook.com>
2024-04-16 08:53:57 +0800

    not working for both dev and build

--- src/canvas/main.tsx
+++ src/canvas/main.tsx
@@ -9,10 +9,9 @@ import WidgetContainer from "../components/WidgetContainer";
 // import "../../default_deps_dist/react"
 // import "../../default_deps_dist/apis";
 
-import * as p from "../@deskulpt/react";
-import * as q from "../@deskulpt/apis";
-console.log(p);
-console.log(q);
+import * as p from "../@deskulpt/react"
+import * as q from "../@deskulpt/apis"
+console.log(p, q);
 
 window.__DESKULPT__ = { defaultDeps: { React } };
 
