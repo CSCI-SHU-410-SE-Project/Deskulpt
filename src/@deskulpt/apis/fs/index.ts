a7d3fefe8652dd3a807ef2a83f524d8d166486d4
Xinyu Li <eigerli@outlook.com>
2024-04-16 20:42:50 +0800

    add more raw ts wrapper for widget api

--- src/@deskulpt/apis/fs/index.ts
+++ src/@deskulpt/apis/fs/index.ts
@@ -1,7 +1,23 @@
 import { invoke } from "@tauri-apps/api";
 
+function exists(widgetId: string, path: string) {
+  return invoke("plugin:widget_api.fs|exists", { widgetId: widgetId, path: path });
+}
+
+function isFile(widgetId: string, path: string) {
+  return invoke("plugin:widget_api.fs|is_file", { widgetId: widgetId, path: path });
+}
+
 function readFile(widgetId: string, path: string) {
   return invoke("plugin:widget_api.fs|read_file", { widgetId: widgetId, path: path });
 }
 
-export { readFile };
+function writeFile(widgetId: string, path: string, content: string) {
+  return invoke("plugin:widget_api.fs|write_file", {
+    widgetId: widgetId,
+    path: path,
+    content: content,
+  });
+}
+
+export { exists, isFile, readFile, writeFile };
