727ede0c2289b414f995bc5202fccda77fb02f05
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-16 15:29:56 +0800

    ignore website from vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
     port: 1420,
     strictPort: true,
     watch: {
-      ignored: ["**/src-tauri/**"],
+      ignored: ["**/src-tauri/**", "website/**"],
     },
   },
   build: {
