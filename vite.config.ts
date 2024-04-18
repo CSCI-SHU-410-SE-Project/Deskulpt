ef91b827afe9da36f96c2abb5f6196ca2196c750
Xinyu Li <eigerli@outlook.com>
2024-04-18 14:03:44 +0800

    can preserve internal import name and structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+function normalizePath(p: string) {
+  return p.replace(/\\/g, "/");
+}
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -25,15 +29,40 @@ export default defineConfig({
         main: resolve(__dirname, "views/index.html"),
         canvas: resolve(__dirname, "views/canvas.html"),
       },
+      // output: {
+      //   preserveModules: true,
+      //   // remove hash from chunk names so that we can explicitly import them
+      //   // in the importmap in views/canvas.html
+      //   entryFileNames: `assets/[name].js`,
+      //   chunkFileNames: `assets/[name].js`,
+      //   assetFileNames: `assets/[name].[ext]`,
+      // },
+      // preserveEntrySignatures: "allow-extension",
       output: {
-        preserveModules: true,
-        // remove hash from chunk names so that we can explicitly import them
-        // in the importmap in views/canvas.html
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+        minifyInternalExports: false,
+        // only keep the strucgture of the @deskulpt/xxx modules
+        manualChunks(id) {
+          // Normalize the incoming module ID and the base path for @deskulpt
+          const normalizedId = normalizePath(id);
+          const deskulptPath = normalizePath(path.resolve(__dirname, "src/@deskulpt"));
+          if (normalizedId.includes(deskulptPath)) {
+            console.log(normalizedId);
+            const pathParts = normalizedId.split("/");
+            const deskulptIndex = pathParts.indexOf("@deskulpt");
+            const specificPath = pathParts
+              .slice(deskulptIndex)
+              .join("/")
+              .replace(/\.tsx?$/, "");
+            return `${specificPath}`;
+          }
+        },
+        chunkFileNames(chunkInfo) {
+          if (chunkInfo.name.startsWith("@deskulpt")) {
+            return `[name].js`;
+          }
+          return `assets/[name]-[hash].js`;
+        },
       },
-      preserveEntrySignatures: "allow-extension",
     },
   },
 });
