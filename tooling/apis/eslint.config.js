35564e737c36fb97af9c91d9a8a14258afb57bd0
Xinyu Li <eigerli@outlook.com>
2024-04-23 13:14:13 +0800

    further relax the eslint checing for @deskulpt-test/apis

--- tooling/apis/eslint.config.js
+++ tooling/apis/eslint.config.js
@@ -47,7 +47,9 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
     },
   },
