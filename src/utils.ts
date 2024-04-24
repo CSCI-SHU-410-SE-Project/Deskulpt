d327dbbebdfed79433c182e6a27097b16d630b2b
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-24 15:10:24 +0800

    some more comments

--- src/utils.ts
+++ src/utils.ts
@@ -17,12 +17,22 @@ export function grabErrorInfo(err: unknown) {
   return "Unknown error caught that is neither a string nor an Error";
 }
 
+/**
+ * Create a blob for widget APIs.
+ *
+ * This function fetches the template of widget APIs and replaces the placeholder with
+ * the actual widget ID. The template essentially refers to raw APIs, converts them to
+ * widget-specific APIs, and exports them. The blob is meant to be wrapped in a URL for
+ * dynamic import using `createObjectURL`.
+ *
+ * @param widgetId The ID of the widget to create APIs for.
+ * @returns The blob object wrapping the widget APIs.
+ */
 export async function createWidgetApisBlob(widgetId: string) {
-  // "/apis.txt" is compiled from the npm package in /tooling/apis
-  // It is a template for the widget's apis
-  const jsCodeResponse = await fetch("/.wrap-apis.js.txt");
-  const jsCodeTemplate = await jsCodeResponse.text();
-  return new Blob([jsCodeTemplate.replace("__DESKULPT_WIDGET_ID__", widgetId)], {
+  // The template is in the public directory, bundled from `tooling/apis`
+  const templateResponse = await fetch("/.wrap-apis.js.txt");
+  const template = await templateResponse.text();
+  return new Blob([template.replace("__DESKULPT_WIDGET_ID__", widgetId)], {
     type: "application/javascript",
   });
 }
