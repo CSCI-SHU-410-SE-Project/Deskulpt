1a6f817aad85278c47275aec3ae4a47d0f888a89
ROMEEZHOU <yz7258@nyu.edu>
2024-05-04 17:13:57 +0800

    a simple about page

--- src/components/AboutTab.tsx
+++ src/components/AboutTab.tsx
@@ -1,6 +1,36 @@
+import Logo from "/deskulpt.svg";
+
 /**
  * The about tab in the manager.
  */
+
 export default function AboutTab() {
-  return <h1>This is the about tab</h1>;
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <div style={{ marginRight: "20px" }}>
+        <img
+          src={Logo}
+          alt="Software Logo"
+          style={{ width: "150px", height: "auto" }}
+        />
+      </div>
+      <div style={{ textAlign: "left" }}>
+        <h1 style={{ fontSize: "24px", marginBottom: "10px" }}>Deskulpt v1.0.0</h1>
+        <p style={{ fontSize: "16px", marginBottom: "10px" }}>
+          Created by: Xinyu Li, Yao Xiao, Yuchen Zhou, Runkai Zhu
+        </p>
+        <p style={{ fontSize: "16px", marginBottom: "10px" }}>
+          A Cross-platform Desktop Customization Tool
+        </p>
+        <a
+          href={"https://csci-shu-410-se-project.github.io/Deskulpt/"}
+          target="_blank"
+          rel="noreferrer"
+          style={{ fontSize: "16px", textDecoration: "none", color: "#007bff" }}
+        >
+          Website Link not working
+        </a>
+      </div>
+    </div>
+  );
 }
