b4428231a75f3beeb3f255f433004e7274c0e396
Xinyu Li <eigerli@outlook.com>
2024-05-12 16:07:25 +0800

    add more entry points to display all contents in the src folder

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+// Re-export all components within single file for easier doc generation
+export { default as About } from "./AboutTab";
+export { default as ErrorDisplay } from "./ErrorDisplay";
+export { default as Logs } from "./LogsTab";
+export { default as WidgetContainer } from "./WidgetContainer";
+export { default as Settings } from "./SettingsTab";
+export { default as Widgets } from "./WidgetsTab";
---
+++ src/hooks/index.tsx
@@ -0,0 +1,7 @@
+// Re-export all components within single file for easier doc generation
+export * from "./useExitAppListener";
+export * from "./useRemoveWidgetsListener";
+export * from "./useRenderWidgetListener";
+export * from "./useShowToastListener";
+export * from "./useToggleShortcut";
+export * from "./useUpdateSettingListener";
