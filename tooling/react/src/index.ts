bdaf5e07e989334d19edf3f912e08427dc01fdab
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-06-08 02:21:42 +0800

    please the linter

--- tooling/react/src/index.ts
+++ tooling/react/src/index.ts
@@ -1,12 +1,14 @@
-import React, {
-  /**
-   * React hooks; https://18.react.dev/reference/react/hooks
-   *
-   * The following hooks are not included:
-   * - useActionState: experimental
-   * - useDebugValue: only related to react devtools which will not be available
-   * - useOptimistic: experimental
-   */
+import React from "react";
+
+/**
+ * React hooks; https://18.react.dev/reference/react/hooks
+ *
+ * The following hooks are not included:
+ * - useActionState: experimental
+ * - useDebugValue: only related to react devtools which will not be available
+ * - useOptimistic: experimental
+ */
+import {
   useCallback,
   useContext,
   useDeferredValue,
@@ -21,30 +23,27 @@ import React, {
   useState,
   useSyncExternalStore,
   useTransition,
-  /**
-   * React components; https://18.react.dev/reference/react/components
-   *
-   * The following components are not included:
-   * - Profiler: disabled in production build by default
-   * - StrictMode: only used for development, and widgets are already wrapped in strict
-   *   mode in the widget canvas
-   */
-  Fragment,
-  Suspense,
-  /**
-   * APIs; https://18.react.dev/reference/react/apis
-   *
-   * The following APIs are not included:
-   * - act: only used for testing
-   * - cache: experimental
-   * - use: experimental
-   */
-  createContext,
-  forwardRef,
-  lazy,
-  memo,
-  startTransition,
 } from "react";
+
+/**
+ * React components; https://18.react.dev/reference/react/components
+ *
+ * The following components are not included:
+ * - Profiler: disabled in production build by default
+ * - StrictMode: only used for development, and widgets are already wrapped in strict
+ *   mode in the widget canvas
+ */
+import { Fragment, Suspense } from "react";
+
+/**
+ * APIs; https://18.react.dev/reference/react/apis
+ *
+ * The following APIs are not included:
+ * - act: only used for testing
+ * - cache: experimental
+ * - use: experimental
+ */
+import { createContext, forwardRef, lazy, memo, startTransition } from "react";
 
 export default React;
 
