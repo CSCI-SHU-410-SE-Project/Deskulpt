7c198b6933127afe56a47f1bd91cf8ba578e964c
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-06-08 20:20:22 +0800

    fix test and labeler

--- src-tauri/tests/fixtures/bundler/external_deps/output.js
+++ src-tauri/tests/fixtures/bundler/external_deps/output.js
@@ -1,1 +1,1 @@
-import{css}from"@deskulpt-test/emotion";import React,{useState}from"@deskulpt-test/react";import{matcher}from"matcher";import osName from"os-name";console.log(apis,css,React,useState,matcher,osName);
+import React,{useState}from"@deskulpt-test/react";import{css}from"@deskulpt-test/ui";import{matcher}from"matcher";import osName from"os-name";console.log(apis,css,React,useState,matcher,osName);
