28b5fbfcba50999366949dd716de340abfc8996a
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-06-15 15:31:48 +0800

    make all named exports available

--- tooling/react/src/index.ts
+++ tooling/react/src/index.ts
@@ -1,72 +1,36 @@
-import React from "react";
-
-/**
- * React hooks; https://18.react.dev/reference/react/hooks
- *
- * The following hooks are not included:
- * - useActionState: experimental
- * - useDebugValue: only related to react devtools which will not be available
- * - useOptimistic: experimental
- */
-import {
-  useCallback,
-  useContext,
-  useDeferredValue,
-  useEffect,
-  useId,
-  useImperativeHandle,
-  useInsertionEffect,
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-  useSyncExternalStore,
-  useTransition,
-} from "react";
-
-/**
- * React components; https://18.react.dev/reference/react/components
- *
- * The following components are not included:
- * - Profiler: disabled in production build by default
- * - StrictMode: only used for development, and widgets are already wrapped in strict
- *   mode in the widget canvas
- */
-import { Fragment, Suspense } from "react";
-
-/**
- * APIs; https://18.react.dev/reference/react/apis
- *
- * The following APIs are not included:
- * - act: only used for testing
- * - cache: experimental
- * - use: experimental
- */
-import { createContext, forwardRef, lazy, memo, startTransition } from "react";
-
-export default React;
-
 export {
-  useCallback,
-  useContext,
-  useDeferredValue,
-  useEffect,
-  useId,
-  useImperativeHandle,
-  useInsertionEffect,
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-  useSyncExternalStore,
-  useTransition,
+  default, // Default React export
+  Children,
+  Component,
   Fragment,
+  Profiler,
+  PureComponent,
+  StrictMode,
   Suspense,
+  cloneElement,
   createContext,
+  createElement,
+  createFactory,
+  createRef,
   forwardRef,
+  isValidElement,
   lazy,
   memo,
   startTransition,
-};
+  useCallback,
+  useContext,
+  useDebugValue,
+  useDeferredValue,
+  useEffect,
+  useId,
+  useImperativeHandle,
+  useInsertionEffect,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  useSyncExternalStore,
+  useTransition,
+  version,
+} from "react";
