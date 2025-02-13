ed4316dd7c3694c699fb72ba57509388ac879db4
Charlie-XIAO <yx2436@nyu.edu>
2025-02-13 01:05:40 -0500

    refactor: about tab

---
+++ src/manager/components/ExternalCopyLink.tsx
@@ -0,0 +1,43 @@
+import { Flex, FlexProps, IconButton, Link, LinkProps } from "@radix-ui/themes";
+import { RxCopy } from "react-icons/rx";
+import { writeText } from "@tauri-apps/plugin-clipboard-manager";
+import { toast } from "sonner";
+import { useCallback } from "react";
+
+interface ExternalCopyLinkProps extends LinkProps {
+  gap?: FlexProps["gap"];
+}
+
+const ExternalCopyLink = ({
+  gap = "2",
+  children,
+  ...linkProps
+}: ExternalCopyLinkProps) => {
+  const handleCopy = useCallback(() => {
+    if (linkProps.href !== undefined) {
+      writeText(linkProps.href).then(() =>
+        toast.success("Copied to clipboard."),
+      );
+    }
+  }, [linkProps.href]);
+
+  return (
+    <Flex gap={gap} align="center">
+      <Link {...linkProps} target="_blank" rel="noreferrer">
+        {children}
+      </Link>
+      {linkProps.href !== undefined && (
+        <IconButton
+          size="1"
+          variant="ghost"
+          title="Copy link"
+          onClick={handleCopy}
+        >
+          <RxCopy />
+        </IconButton>
+      )}
+    </Flex>
+  );
+};
+
+export default ExternalCopyLink;
