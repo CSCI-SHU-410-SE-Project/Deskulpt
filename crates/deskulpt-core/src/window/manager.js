c250fd0c179343a8c877e69a2ebbc1cb508baa02
Charlie-XIAO <yx2436@nyu.edu>
2025-08-29 16:03:48 -0400

    fix: window initialization scripts invalid properties

--- crates/deskulpt-core/src/window/manager.js
+++ crates/deskulpt-core/src/window/manager.js
@@ -7,9 +7,9 @@ Object.defineProperty(window, "__DESKULPT_MANAGER_INTERNALS__", {
   enumerable: false,
 });
 
-const props = window.__DESKULPT_CANVAS_INTERNALS__;
+const props = window.__DESKULPT_MANAGER_INTERNALS__;
 Object.freeze(props);
 Object.freeze(props.initialSettings);
-Object.values(props.initialSettings.widgetSettingsMap).forEach((value) => {
+Object.values(props.initialSettings.widgets).forEach((value) => {
   Object.freeze(value);
 });
