8ddf1efef6d0a88455915f4fe189868091edb99e
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2025-10-15 14:53:30 -0400

    fix: cannot resolve the generated modules with pnpm tauri dev (#580)

--- packages/react/rollup.config.ts
+++ packages/react/rollup.config.ts
@@ -15,7 +15,7 @@ export default defineConfig([
     input: "src/index.ts",
     output: {
       format: "esm",
-      file: "../deskulpt/src/gen/react.js",
+      file: "../deskulpt/gen/react.js",
       banner: "/*! Auto-generated from packages/react. DO NOT EDIT! */",
     },
     external: ["react"],
